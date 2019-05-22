f9eee2361566159a98ffb01037aa4aa939474ecc
christophe gueroult <christophegueroult@gmail.com>
2019-05-22 15:38:13 +0200

    directive perso

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { TotalPipe } from './pipes/total.pipe';
+import { StateDirective } from './directives/state.directive';
 
 @NgModule({
-  declarations: [TotalPipe],
-  exports: [TotalPipe],
+  declarations: [TotalPipe, StateDirective],
+  exports: [TotalPipe, StateDirective],
   imports: [
     CommonModule
   ]
